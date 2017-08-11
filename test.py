93eb64f7f74f9ce689c5908ca7926a986032cc4c
Theo Budiyanto <tbudiyanto@splunk.com>
2017-08-10 19:53:21 -0700

    init

---
+++ test.py
@@ -0,0 +1,46 @@
+import requests
+import unittest
+
+
+BASE_URL = 'localhost'
+PORT = '8000'
+ENDPOINT = 'notes'
+
+class TestEndpoint(unittest.TestCase):
+    title =  "test title",
+    message = "This is just a title string"
+
+    def test_post_note(self):
+        payload = {
+                "title": title,
+                "message": message
+            }
+        r = requests.post(BASE_URL + ":" + PORT + "/" + ENDPOINT, json=payload)
+        try:
+            data = r.json()
+            self.assertEqual(int(data['code']), 200)
+        except:
+            self.assertTrue(False)
+
+    def test_get_note(self):
+        r = requests.post(BASE_URL + ":" + PORT + "/" + ENDPOINT)
+        try:
+            data = r.json()
+            self.assertGreater(len(data), 0)
+        except:
+            self.assertTrue(False)
+
+    def test_get_title(self):
+        payload = {
+                "title": title
+            }
+        r = requests.post(BASE_URL + ":" + PORT + "/" + ENDPOINT, json=payload)
+        try:
+            data = r.json()
+            self.assertEqual((data['title']), title)
+            self.assertEqual((data['message']), message)
+        except:
+            self.assertTrue(False)
+
+if __name__ == '__main__':
+    unittest.main()
